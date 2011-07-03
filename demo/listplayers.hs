bb6a49644d7348a261484268c93e0c9b7b1c5204
Christoffer Öjeling <cadynum@gmail.com>
2011-07-03 17:08:07 +0200

    Fixed listplayers

--- demo/listplayers.hs
+++ demo/listplayers.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE RecordWildCards #-}
 import Network.Socket
-import Tremulous.Protocol
-import Tremulous.Polling
-import Tremulous.Util
+import Network.Tremulous.Protocol
+import Network.Tremulous.Polling
+import Network.Tremulous.Util
 import Control.Monad
 
 delay = Delay (400*1000) 3 (0)
@@ -16,12 +16,9 @@ getDNS host port =  do
 main = withSocketsDo $ do
 	host	<- getDNS "master.tremulous.net" "30710"
 	host2	<- getDNS "master.tremulous.net" "30700"
-	polled	<- pollMasters delay	[ MasterServer 69 host
+	PollMasters{..}	<- pollMasters delay	[ MasterServer 69 host
 					, MasterServer 70 host2 ]
 
-	print $ length polled
-	--print $ map fmt polledd
-	--mapM_ (\Player{..} -> putStrLn $
-	--	unpackorig name ++ "\t\t" ++  unpack (cleanedCase name) )  (playerList polled)
+	print $ serversResponded
 
 
