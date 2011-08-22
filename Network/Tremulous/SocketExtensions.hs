2eafe5071ed2df61894eeee76be0294f211b254b
Christoffer Öjeling <cadynum@gmail.com>
2011-08-22 04:41:35 +0200

    Windows fix

--- Network/Tremulous/SocketExtensions.hs
+++ Network/Tremulous/SocketExtensions.hs
@@ -11,7 +11,9 @@ deriving instance Ord SockAddr
 instance NFData SockAddr where
 	rnf (SockAddrInet (PortNum a) b) = rnf a `seq` rnf b
 	rnf (SockAddrInet6 (PortNum a) b c d) = rnf a `seq` rnf b `seq` rnf c `seq` rnf d
+#if !defined(mingw32_HOST_OS) && !defined(__MINGW32__)
 	rnf (SockAddrUnix a) = rnf a
+#endif
 
 foreign import ccall unsafe "ntohl" ntohl :: Word32 -> Word32
 foreign import ccall unsafe "htonl" htonl :: Word32 -> Word32
